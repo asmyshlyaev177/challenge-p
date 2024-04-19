86411a37f1255bac569857965d747d4f49970513
Aleksandr Smyshliaev <asmyshlyaev177@gmail.com>
2024-04-19 13:47:47 +0400

    POC and working tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,115 @@
+import dayjs from 'dayjs';
+import weekOfYear from 'dayjs/plugin/weekOfYear.js';
+
 import checkFile from './checkFile.mjs';
+
+dayjs.extend(weekOfYear);
 
 const args = process.argv.slice(2);
 
 const file = checkFile(args);
 
-console.log({ file });
+const currencies = { EUR: { fractions: 2 } };
+
+const eurConfig = {
+  natural: {
+    cash_in: {
+      percents: 0.03,
+      max: {
+        amount: 5,
+        currency: 'EUR',
+      },
+    },
+    cash_out: {
+      percents: 0.3,
+
+      week_limit: {
+        amount: 1000,
+        currency: 'EUR',
+      },
+    },
+  },
+  juridical: {
+    cash_in: {
+      percents: 0.03,
+      max: {
+        amount: 5,
+        currency: 'EUR',
+      },
+    },
+    cash_out: {
+      percents: 0.3,
+      min: {
+        amount: 0.5,
+        currency: 'EUR',
+      },
+    },
+  },
+};
+
+class FeeManager {
+  constructor() {
+    this.config = {};
+    this.users = {};
+  }
+
+  // TODO: to separate function with tests?
+  parseRule = (operationConfig) => {
+    return (rowObj) => {
+      let toFee = rowObj.operation.amount;
+
+      if (operationConfig.week_limit) {
+        const date = dayjs(rowObj.date);
+        const limitKey = `${rowObj.user_id}-${date.year()}-${date.week()}`;
+
+        const remainingLimit =
+          this.users?.[limitKey] ?? operationConfig.week_limit.amount;
+
+        this.users[limitKey] = Math.max(remainingLimit - toFee, 0);
+
+        toFee = Math.max(toFee - remainingLimit, 0);
+      }
+
+      return [
+        operationConfig.percents &&
+          ((val) => (val * operationConfig.percents) / 100),
+        operationConfig.min &&
+          ((val) => Math.max(val, operationConfig.min.amount)),
+        operationConfig.max &&
+          ((val) => Math.min(val, operationConfig.max.amount)),
+        (val) =>
+          Number(val).toFixed(currencies[rowObj.operation.currency].fractions),
+      ]
+        .filter(Boolean)
+        .reduce((acc, fn) => fn(acc), toFee);
+    };
+  };
+
+  run = (rows) => {
+    return rows.reduce((acc, row) => {
+      const res = this.config[row.user_type][row.type](row);
+      acc.push({ ...row, fee: res });
+      return acc;
+    }, []);
+  };
+
+  setConfig = (config) => {
+    this.config = {
+      natural: {
+        cash_in: this.parseRule(config.natural.cash_in),
+        cash_out: this.parseRule(config.natural.cash_out),
+      },
+      juridical: {
+        cash_in: this.parseRule(config.juridical.cash_in),
+        cash_out: this.parseRule(config.juridical.cash_out),
+      },
+    };
+  };
+}
+
+const feeManager = new FeeManager();
+feeManager.setConfig(eurConfig);
+
+const result = feeManager.run(file);
+
+result.map((el) => el.fee).forEach((fee) => console.log(fee));
--- src/index.test.js
+++ src/index.test.js
@@ -1,47 +1,79 @@
 const cp = require('child_process');
 
+const execCommand = async (command = '') => {
+  return new Promise((resolve, reject) => {
+    const proc = cp.exec(command);
+
+    let stdout = '';
+    let stderr = '';
+
+    proc.stdout?.on('data', (data) => {
+      stdout += data.toString();
+    });
+
+    proc.stderr?.on('data', (data) => {
+      stderr += data.toString();
+    });
+
+    proc.on('close', (code) => {
+      resolve({ stdout, stderr, code });
+    });
+
+    proc.on('error', (err) => {
+      reject(err);
+    });
+  });
+};
+
 describe('app cli', () => {
   describe('file argument', () => {
-    it('Should throw an error if file not provided', () => {
-      cp.exec('npm run app', (error, stdout, stderr) => {
-        expect(stderr).toMatch('Please provide JSON file');
-        expect(error).toBeTruthy();
-        expect(stdout).toBeTruthy();
-      }).on('exit', (code) => {
-        expect(code).toEqual(1);
-      });
+    it('Should throw an error if file not provided', async () => {
+      const res = await execCommand('npm run app');
+      expect(res.code).toEqual(1);
+      expect(res.stderr).toMatch('Please provide JSON file');
     });
 
-    it('No error if file provided', () => {
-      cp.exec('npm run app input.json', (error, stdout, stderr) => {
-        expect(stderr).toBeFalsy();
-        expect(error).toBeFalsy();
-        expect(stdout).toBeTruthy();
-      }).on('exit', (code) => {
-        expect(code).toEqual(0);
-      });
+    it('No error if file provided', async () => {
+      const res = await execCommand('npm run app input.json');
+      expect(res.code).toEqual(0);
+      expect(res.stderr).toBeFalsy();
     });
 
-    it('Error if file does not exists', () => {
+    it('Error if file does not exists', async () => {
       const file = 'package.json11';
-      cp.exec(`npm run app ${file}`, (error, stdout, stderr) => {
-        expect(stderr).toMatch(`File "${file}" doesn't exist`);
-        expect(error).toBeTruthy();
-        expect(stdout).toBeTruthy();
-      }).on('exit', (code) => {
-        expect(code).toEqual(1);
-      });
+
+      const res = await execCommand(`npm run app ${file}`);
+      expect(res.code).toEqual(1);
+      expect(res.stderr).toMatch(`File "${file}" doesn't exist`);
     });
 
-    it('Error if file is not a .json', () => {
+    it('Error if file is not a .json', async () => {
       const file = 'README.md';
-      cp.exec(`npm run app ${file}`, (error, stdout, stderr) => {
-        expect(stderr).toMatch(`Please provide JSON file`);
-        expect(error).toBeTruthy();
-        expect(stdout).toBeTruthy();
-      }).on('exit', (code) => {
-        expect(code).toEqual(1);
-      });
+
+      const res = await execCommand(`npm run app ${file}`);
+      expect(res.code).toEqual(1);
+      expect(res.stderr).toMatch(`Please provide JSON file`);
+    });
+  });
+
+  describe('should return result', () => {
+    it('correct result', async () => {
+      const expected = [
+        '0.06',
+        '0.90',
+        '87.00',
+        '3.00',
+        '0.30',
+        '0.30',
+        '5.00',
+        '0.00',
+        '0.00',
+      ];
+
+      const res = await execCommand('npm run app input.json');
+      expect(res.code).toEqual(0);
+      expect(res.stderr).toBeFalsy();
+      expect(res.stdout.split('\n').filter(Boolean).slice(2)).toEqual(expected);
     });
   });
 });
